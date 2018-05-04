f3bc00c07e65f8b780e4999ff5e269c0cb322b04
lucaszw <lucas.zeer@gmail.com>
2018-05-04 17:16:35 -0400

    remove comments

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const {MicropedeClient} = require('@micropede/client/src/client.js');
-const MicropedeAsync = require('@micropede/client/src/async.js');
 
 const five = require("johnny-five");
 const _ = require('lodash');
@@ -30,7 +29,6 @@ class FerroBotDriver extends MicropedeClient {
     });
   }
   async turnOn(electrodeIds) {
-    console.log("turning on: ", electrodeIds);
 
     // Turn off all electrodes
     _.each(_.range(0,6), (row) => {
@@ -40,13 +38,6 @@ class FerroBotDriver extends MicropedeClient {
       });
     });
 
-    // const scicad = new MicropedeAsync(APPNAME, undefined, this.port);
-
-    // _.each(this.prevElectrodes, (electrode) => {
-    //   board.digitalWrite(ROWS[_.get(electrode, "row")], 0);
-    //   board.digitalWrite(COLS[_.get(electrode, "col")], 0);
-    // });
-
     _.each(electrodeIds, (electrodeId) => {
       const row = parseInt(electrodeId.split("electrode")[1][ROW]);
       const col = parseInt(electrodeId.split("electrode")[1][COL]);
@@ -54,24 +45,6 @@ class FerroBotDriver extends MicropedeClient {
       board.digitalWrite(COLS[col], 1);
     });
 
-    // // Turn off previous row and column
-    // if (this.prevRow != null && this.prevCol != null) {
-    //   board.digitalWrite(ROWS[this.prevRow], 0);
-    //   board.digitalWrite(COLS[this.prevCol], 0);
-    // }
-
-    // Turn on electrode new electrode based on electrode id
-    // const row = parseInt(electrodeId.split("electrode")[1][ROW]);
-    // const col = parseInt(electrodeId.split("electrode")[1][COL]);
-
-    // console.log({row, col});
-    //
-    // this.prevRow = row;
-    // this.prevCol = col;
-    //
-    // board.digitalWrite(ROWS[row], 1);
-    // board.digitalWrite(COLS[col], 1);
-    // scicad.putPlugin('electrodes-model', 'active-electrodes', []);
   }
 
 }
